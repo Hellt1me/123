226b98dc4922e59115ad3d031edcfd18b046f3b2
Vlad Chembarskikh <chembarskihvlad@gmail.com>
2024-10-21 11:33:51 +0500

    2 zadanie

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,64 +21,41 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
-            statusTextBlock.Text = "Приложение запущено";
 
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void Color_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-                mi_open.Background = Brushes.Aquamarine;
-            statusTextBlock.Text = "Фон сменен";
+
         }
 
-
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        private void Edit_Checked(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Чембарских В.А");
-            statusTextBlock.Text = "Данные выведены";
+
         }
 
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        private void Edit_Checked_1(object sender, RoutedEventArgs e)
         {
-            this.Close();
+
         }
 
-        private void MenuItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void Доска_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            mi_open.Background = Brushes.White;
+
         }
 
-        private void MenuItem_MouseEnter(object sender, MouseEventArgs e)
+        private void Доска_MouseMove(object sender, MouseEventArgs e)
         {
-            statusTextBlock.Text = "Элемент меню";
+
         }
 
-        private void MenuItem_MouseLeave(object sender, MouseEventArgs e)
+        private void Доска_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            statusTextBlock.Text = " ";
-        }
 
-        private void MenuItem_MouseEnter_1(object sender, MouseEventArgs e)
-        {
-            statusTextBlock.Text = "Элемент меню";
-        }
-
-        private void MenuItem_MouseLeave_1(object sender, MouseEventArgs e)
-        {
-            statusTextBlock.Text = " ";
-        }
-
-        private void MenuItem_MouseEnter_2(object sender, MouseEventArgs e)
-        {
-            statusTextBlock.Text = "Элемент меню";
-        }
-
-        private void MenuItem_MouseLeave_2(object sender, MouseEventArgs e)
-        {
-            statusTextBlock.Text = " ";
         }
     }
 }
